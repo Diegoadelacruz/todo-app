49a8dcd566e8e86846b9e5de442b28f6df23d10f
Diego de la Cruz <diego.dlacruz@gmail.com>
2024-04-28 18:51:57 -0600

    Add FormTodo functions

--- src/components/FormTodo.jsx
+++ src/components/FormTodo.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const FormTodo = () => {
+const FormTodo = props => {
+  const { handleAddItem } = props;
+  const [description, setDescription] = useState("");
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    handleAddItem({
+      done: false,
+      id: (+new Date()).toString(),
+      description
+    });
+
+    setDescription("");
+  };
   return (
-    <div>FormTodo</div>
+    <form onSubmit={handleSubmit}>
+      <div className='todo-list'>
+        <div className='file-input'>
+
+          <input
+            type="text"
+            className='text'
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
+          <button
+            className='button pink'
+            disabled={description ? "" : "disabled"}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </form>
   )
 }
 
